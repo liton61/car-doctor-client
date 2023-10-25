3f483c6b56b237f5e67e6a0a9da8d6535ebb035b
liton61 <litonofficial61@gmail.com>
2023-10-25 21:09:28 +0600

    footer added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-gray-100">
+            <div className="navbar p-10 bg-base-200">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
